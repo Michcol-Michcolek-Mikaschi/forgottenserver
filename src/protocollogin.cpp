433093d7f23bb6e9623dfbd8f3327b7bec8dc134
Michal <126108277+Michcol-Michcolek-Mikaschi@users.noreply.github.com>
2025-11-22 17:39:39 +0100

    Michal
    Naprawa Attack Speed

--- src/protocollogin.cpp
+++ src/protocollogin.cpp
@@ -121,7 +121,7 @@ void ProtocolLogin::getCharacterList(const std::string& accountName, const std::
 	if (!db.executeQuery(
 	        fmt::format("INSERT INTO `sessions` (`token`, `account_id`, `ip`) VALUES ({:s}, {:d}, INET6_ATON({:s}))",
 	                    db.escapeBlob(sessionKey.data(), sessionKey.size()), id,
-	                    "'" + db.escapeString(connection->getIP().to_string()) + "'"))) {
+	                    db.escapeString(connection->getIP().to_string())))) {
 		disconnectClient("Failed to create session.\nPlease try again later.", version);
 		return;
 	}
